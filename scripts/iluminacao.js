414717cfd6c22b8748a935824e1fdb188b146bef
Navarone <brunohenrique.travessa@gmail.com>
2023-05-21 11:47:36 -0300

    add a pasta e o arquivo de iluminao

---
+++ scripts/iluminacao.js
@@ -0,0 +1,7 @@
+const corpo_Html = document.querySelector('html');
+const botao_iluminacao = document.getElementById('botao-iluminacao')
+
+botao_iluminacao.addEventListener('click', ()=> {
+    console.log("Tic");
+    corpo_Html.classList.toggle('darkMode-LightMode')
+})
