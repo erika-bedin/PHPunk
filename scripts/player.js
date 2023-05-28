4fc8a60a900b70b6e8228210579f2a4482022d86
Érika Bedin <erikabedin.administradora@gmail.com>
2023-05-28 17:20:01 -0300

    Alterações Sprint IV Érika

--- scripts/player.js
+++ scripts/player.js
@@ -1,23 +1,36 @@
-// Variável global para o objeto do player
-var player;
+// Obtenha todos os iframes de vídeo
+const videoFrames = document.querySelectorAll('iframe');
 
-// Função de inicialização do player
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: 'VIDEO_ID', // Substitua pelo ID do vídeo desejado
-    events: {
-      'onReady': onPlayerReady
+// Variável para controlar o índice atual do vídeo
+let currentVideoIndex = 0;
+
+// Função para reproduzir o próximo vídeo
+function playNextVideo() {
+    // Pausa o vídeo atual
+    videoFrames[currentVideoIndex].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+
+    // Incrementa o índice
+    currentVideoIndex++;
+
+    // Verifica se chegou ao fim da lista de vídeos
+    if (currentVideoIndex >= videoFrames.length) {
+        // Reinicia o índice para repetir a reprodução
+        currentVideoIndex = 0;
     }
-  });
+
+    // Inicia o próximo vídeo
+    videoFrames[currentVideoIndex].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
 }
 
-// Função executada quando o player está pronto
-function onPlayerReady(event) {
-  // Reproduz o vídeo
-  player.playVideo();
+// Adiciona um ouvinte de evento quando um vídeo termina de reproduzir
+function onVideoEnded() {
+    playNextVideo();
 }
 
-// Função para pausar o vídeo
-function pauseVideo() {
-  player.pauseVideo();
-}
+// Adiciona o ouvinte de evento para cada vídeo
+videoFrames.forEach((frame) => {
+    frame.addEventListener('ended', onVideoEnded);
+});
+
+// Inicia a reprodução do primeiro vídeo
+videoFrames[currentVideoIndex].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
