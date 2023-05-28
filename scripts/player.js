c5148bad57431239798468cb077876a6f9c700f0
Érika Bedin <erikabedin.administradora@gmail.com>
2023-05-28 17:38:45 -0300

    Alterações Sprint IV Érika

--- scripts/player.js
+++ scripts/player.js
@@ -1,60 +1,4 @@
-// Variáveis globais para armazenar o objeto do player e o índice atual do vídeo
-let player;
-let currentVideoIndex = 0;
-
-// Função para criar o objeto do player do YouTube
-function onYouTubeIframeAPIReady() {
-  // Cria o objeto do player
-  player = new YT.Player('videoPlayer', {
-    height: '360',
-    width: '640',
-    playerVars: {
-      'autoplay': 1,
-      'controls': 0,
-      'rel': 0,
-      'showinfo': 0
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-// Função chamada quando o player do YouTube estiver pronto
-function onPlayerReady(event) {
-  // Inicia a reprodução do primeiro vídeo
-  event.target.playVideo();
-}
-
-// Função chamada quando o estado do player do YouTube mudar
-function onPlayerStateChange(event) {
-  // Verifica se o vídeo atual terminou de reproduzir
-  if (event.data === YT.PlayerState.ENDED) {
-    // Chama a função para reproduzir o próximo vídeo
-    playNextVideo();
-  }
-}
-
-// Função para reproduzir o próximo vídeo
-function playNextVideo() {
-  // Pausa o vídeo atual
-  player.pauseVideo();
-
-  // Incrementa o índice
-  currentVideoIndex++;
-
-  // Verifica se chegou ao fim da lista de vídeos
-  if (currentVideoIndex >= videoList.length) {
-    // Reinicia o índice para repetir a reprodução
-    currentVideoIndex = 0;
-  }
-
-  // Carrega e reproduz o próximo vídeo
-  player.loadVideoById(videoList[currentVideoIndex]);
-}
-
-// Array com os IDs dos vídeos a serem reproduzidos
+// Array contendo os IDs dos vídeos a serem reproduzidos
 const videoList = [
   '6NXnxTNIWkc',
   'Lo2qQmj0_h4',
@@ -70,5 +14,68 @@ const videoList = [
   'nrIPxlFzDi0'
 ];
 
-// Chama a função para carregar a API do YouTube
-onYouTubeIframeAPIReady();
+// Variável para controlar o índice atual do vídeo
+let currentVideoIndex = 0;
+
+// Função para criar o player do YouTube
+function createYouTubePlayer(videoId) {
+  return new YT.Player('videoPlayer', {
+    height: '360',
+    width: '640',
+    videoId: videoId,
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
+}
+
+// Função chamada quando o player do YouTube estiver pronto
+function onPlayerReady(event) {
+  // Inicia a reprodução do primeiro vídeo
+  event.target.playVideo();
+}
+
+// Função chamada quando o estado do player do YouTube mudar
+function onPlayerStateChange(event) {
+  if (event.data === YT.PlayerState.ENDED) {
+    // Vídeo terminou de reproduzir, passa para o próximo vídeo
+    playNextVideo();
+  }
+}
+
+// Função para reproduzir o próximo vídeo
+function playNextVideo() {
+  // Pausa o vídeo atual
+  player.stopVideo();
+
+  // Incrementa o índice
+  currentVideoIndex++;
+
+  // Verifica se chegou ao fim da lista de vídeos
+  if (currentVideoIndex >= videoList.length) {
+    // Reinicia o índice para repetir a reprodução
+    currentVideoIndex = 0;
+  }
+
+  // Carrega e reproduz o próximo vídeo
+  player.loadVideoById(videoList[currentVideoIndex]);
+}
+
+// Carrega a API do YouTube e inicia a reprodução do primeiro vídeo
+function loadYouTubeAPI() {
+  // Cria um script para carregar a API do YouTube
+  const tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Chama a função onYouTubeIframeAPIReady quando a API estiver pronta
+  window.onYouTubeIframeAPIReady = function() {
+    // Cria o player do YouTube com o primeiro vídeo
+    player = createYouTubePlayer(videoList[currentVideoIndex]);
+  };
+}
+
+// Carrega a API do YouTube ao carregar a página
+loadYouTubeAPI();
