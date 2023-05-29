4a970b752b9740784ddce7a416b262df281898f0
Érika Bedin <erikabedin.administradora@gmail.com>
2023-05-29 12:13:31 -0300

    Alterações Sprint IV Érika

--- scripts/player.js
+++ scripts/player.js
@@ -1,65 +1,40 @@
-  let players = [];
-  let currentVideoIndex = 0;
+  // Variáveis globais
+let player;
 
-  function onYouTubeIframeAPIReady() {
-    players.push(createPlayer('youtube-player1', 'https://www.youtube.com/embed/6NXnxTNIWkc', 0));
-    players.push(createPlayer('youtube-player2', 'https://www.youtube.com/embed/Lo2qQmj0_h4', 2));
-    players.push(createPlayer('youtube-player3', 'https://www.youtube.com/embed/JkK8g6FMEXE', 4));
-    players.push(createPlayer('youtube-player4', 'https://www.youtube.com/embed/TAqZb52sgpU', 6));
-
-    players.push(createPlayer('youtube-player-mais-ouvidos1', 'https://www.youtube.com/embed/9BMwcO6_hyA', 8));
-    players.push(createPlayer('youtube-player-mais-ouvidos2', 'https://www.youtube.com/embed/CD-E-LDc384', 10));
-    players.push(createPlayer('youtube-player-mais-ouvidos3', 'https://www.youtube.com/embed/8SbUC-UaAxE', 12));
-    players.push(createPlayer('youtube-player-mais-ouvidos4', 'https://www.youtube.com/embed/bWXazVhlyxQ', 14));
-
-    players.push(createPlayer('youtube-player-feito-para-voce1', 'https://www.youtube.com/embed/rn_YodiJO6k', 16));
-    players.push(createPlayer('youtube-player-feito-para-voce2', 'https://www.youtube.com/embed/egMWlD3fLJ8', 18));
-    players.push(createPlayer('youtube-player-feito-para-voce3', 'https://www.youtube.com/embed/6Ejga4kJUts', 20));
-    players.push(createPlayer('youtube-player-feito-para-voce4', 'https://www.youtube.com/embed/nrIPxlFzDi0', 22));
-
-    players[currentVideoIndex].playVideo();
-  }
-
-  function createPlayer(playerId, videoUrl, autoplayDelay) {
-    return new YT.Player(playerId, {
-      height: '360',
-      width: '640',
-      videoId: getVideoId(videoUrl),
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      },
-      playerVars: {
-        'autoplay': 0,
-        'controls': 1,
-        'disablekb': 1,
-        'fs': 0,
-        'iv_load_policy': 3,
-        'modestbranding': 1,
-        'rel': 0,
-        'showinfo': 0,
-        'start': autoplayDelay
-      }
-    });
-  }
-
-  function onPlayerReady(event) {
-    if (currentVideoIndex < players.length - 1) {
-      players[currentVideoIndex].cueVideoById(players[currentVideoIndex + 1].getVideoData().video_id);
+// Função chamada quando a API do YouTube estiver pronta
+function onYouTubeIframeAPIReady() {
+  // Cria o player do YouTube
+  player = new YT.Player('player', {
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
-  }
+  });
+}
 
-  function onPlayerStateChange(event) {
-    if (event.data === YT.PlayerState.ENDED) {
-      players[currentVideoIndex].stopVideo();
-      currentVideoIndex++;
-      if (currentVideoIndex < players.length) {
-        players[currentVideoIndex].playVideo();
-      }
-    }
-  }
+// Função chamada quando o player estiver pronto
+function onPlayerReady(event) {
+  // Inicia a reprodução do primeiro vídeo
+  event.target.playVideo();
+}
 
-  function getVideoId(url) {
-    const match = url.match(/(?:\/|%3D|v=|vi=)([0-9A-Za-z-_]{11})(?:[%#?&]|$)/);
-    return match ? match[1] : null;
+// Função chamada quando o estado do player mudar
+function onPlayerStateChange(event) {
+  if (event.data === YT.PlayerState.ENDED) {
+    // Vídeo terminou de reproduzir, passa para o próximo vídeo
+    playNextVideo();
   }
+}
+
+// Função para reproduzir o próximo vídeo
+function playNextVideo() {
+  // Pausa o vídeo atual
+  player.pauseVideo();
+
+  // Altere o VIDEO_ID para o ID do próximo vídeo
+  const nextVideoId = 'NEW_VIDEO_ID';
+
+  // Carrega e reproduz o próximo vídeo
+  player.loadVideoById(nextVideoId);
+  player.playVideo();
+}
