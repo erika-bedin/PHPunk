bc837954d3ba84b0f7db46f79676272f5019abcd
Érika Bedin <erikabedin.administradora@gmail.com>
2023-05-29 12:23:56 -0300

    Alterações Sprint IV Érika

--- scripts/player.js
+++ scripts/player.js
@@ -1,40 +1,40 @@
   // Variáveis globais
-let player;
+  let player;
 
-// Função chamada quando a API do YouTube estiver pronta
-function onYouTubeIframeAPIReady() {
-  // Cria o player do YouTube
-  player = new YT.Player('player', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-// Função chamada quando o player estiver pronto
-function onPlayerReady(event) {
-  // Inicia a reprodução do primeiro vídeo
-  event.target.playVideo();
-}
-
-// Função chamada quando o estado do player mudar
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.ENDED) {
-    // Vídeo terminou de reproduzir, passa para o próximo vídeo
-    playNextVideo();
+  // Função chamada quando a API do YouTube estiver pronta
+  function onYouTubeIframeAPIReady() {
+    // Cria o player do YouTube
+    player = new YT.Player('player', {
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
   }
-}
-
-// Função para reproduzir o próximo vídeo
-function playNextVideo() {
-  // Pausa o vídeo atual
-  player.pauseVideo();
-
-  // Altere o VIDEO_ID para o ID do próximo vídeo
-  const nextVideoId = 'NEW_VIDEO_ID';
-
-  // Carrega e reproduz o próximo vídeo
-  player.loadVideoById(nextVideoId);
-  player.playVideo();
-}
+  
+  // Função chamada quando o player estiver pronto
+  function onPlayerReady(event) {
+    // Inicia a reprodução do primeiro vídeo
+    event.target.playVideo();
+  }
+  
+  // Função chamada quando o estado do player mudar
+  function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+      // Vídeo terminou de reproduzir, passa para o próximo vídeo
+      playNextVideo();
+    }
+  }
+  
+  // Função para reproduzir o próximo vídeo
+  function playNextVideo() {
+    // Pausa o vídeo atual
+    player.pauseVideo();
+  
+    // Altere o VIDEO_ID para o ID do próximo vídeo
+    const nextVideoId = 'NEW_VIDEO_ID';
+  
+    // Carrega e reproduz o próximo vídeo
+    player.loadVideoById(nextVideoId);
+    player.playVideo();
+  }
