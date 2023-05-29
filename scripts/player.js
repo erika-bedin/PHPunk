394073c79104b3f002baed552bc56b6c852d009e
Érika Bedin <erikabedin.administradora@gmail.com>
2023-05-29 11:22:15 -0300

    Alterações Sprint IV Érika

--- scripts/player.js
+++ scripts/player.js
@@ -5,9 +5,9 @@ function loadYouTubeAPI() {
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
-let players = []; 
+let players = [];
 
-function createPlayer(playerId, videoUrl) {
+function createPlayer(playerId, videoUrl, autoplayDelay) {
   return new YT.Player(playerId, {
     height: '360',
     width: '640',
@@ -15,25 +15,36 @@ function createPlayer(playerId, videoUrl) {
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      'autoplay': 0,
+      'controls': 1,
+      'disablekb': 1,
+      'fs': 0,
+      'iv_load_policy': 3,
+      'modestbranding': 1,
+      'rel': 0,
+      'showinfo': 0,
+      'start': autoplayDelay
     }
   });
 }
 
 function onYouTubeIframeAPIReady() {
-  players.push(createPlayer('youtube-player1', 'https://www.youtube.com/embed/6NXnxTNIWkc?autoplay=1'));
-  players.push(createPlayer('youtube-player2', 'https://www.youtube.com/embed/Lo2qQmj0_h4?autoplay=2'));
-  players.push(createPlayer('youtube-player3', 'https://www.youtube.com/embed/JkK8g6FMEXE?autoplay=3'));
-  players.push(createPlayer('youtube-player4', 'https://www.youtube.com/embed/TAqZb52sgpU?autoplay=4'));
+  players.push(createPlayer('youtube-player1', 'https://www.youtube.com/embed/6NXnxTNIWkc', 0));
+  players.push(createPlayer('youtube-player2', 'https://www.youtube.com/embed/Lo2qQmj0_h4', 2));
+  players.push(createPlayer('youtube-player3', 'https://www.youtube.com/embed/JkK8g6FMEXE', 4));
+  players.push(createPlayer('youtube-player4', 'https://www.youtube.com/embed/TAqZb52sgpU', 6));
 
-  players.push(createPlayer('youtube-player-mais-ouvidos1', 'https://www.youtube.com/embed/9BMwcO6_hyA?autoplay=5'));
-  players.push(createPlayer('youtube-player-mais-ouvidos2', 'https://www.youtube.com/embed/CD-E-LDc384?autoplay=6'));
-  players.push(createPlayer('youtube-player-mais-ouvidos3', 'https://www.youtube.com/embed/8SbUC-UaAxE?autoplay=7'));
-  players.push(createPlayer('youtube-player-mais-ouvidos4', 'https://www.youtube.com/embed/bWXazVhlyxQ?autoplay=8'));
+  players.push(createPlayer('youtube-player-mais-ouvidos1', 'https://www.youtube.com/embed/9BMwcO6_hyA', 8));
+  players.push(createPlayer('youtube-player-mais-ouvidos2', 'https://www.youtube.com/embed/CD-E-LDc384', 10));
+  players.push(createPlayer('youtube-player-mais-ouvidos3', 'https://www.youtube.com/embed/8SbUC-UaAxE', 12));
+  players.push(createPlayer('youtube-player-mais-ouvidos4', 'https://www.youtube.com/embed/bWXazVhlyxQ', 14));
 
-  players.push(createPlayer('youtube-player-feito-para-voce1', 'https://www.youtube.com/embed/rn_YodiJO6k?autoplay=9'));
-  players.push(createPlayer('youtube-player-feito-para-voce2', 'https://www.youtube.com/embed/egMWlD3fLJ8?autoplay=10'));
-  players.push(createPlayer('youtube-player-feito-para-voce3', 'https://www.youtube.com/embed/6Ejga4kJUts?autoplay=11'));
-  players.push(createPlayer('youtube-player-feito-para-voce4', 'https://www.youtube.com/embed/nrIPxlFzDi0?autoplay=12'));
+  players.push(createPlayer('youtube-player-feito-para-voce1', 'https://www.youtube.com/embed/rn_YodiJO6k', 16));
+  players.push(createPlayer('youtube-player-feito-para-voce2', 'https://www.youtube.com/embed/egMWlD3fLJ8', 18));
+  players.push(createPlayer('youtube-player-feito-para-voce3', 'https://www.youtube.com/embed/6Ejga4kJUts', 20));
+  players.push(createPlayer('youtube-player-feito-para-voce4', 'https://www.youtube.com/embed/nrIPxlFzDi0', 22));
 }
 
 function onPlayerReady(event) {
